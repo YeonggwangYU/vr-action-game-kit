93160d4a65ae390408caeb816bbeb58ce48e17b5
@_mathken <11295237+mathken029@users.noreply.github.com>
2023-03-31 19:35:54 +0900

    Revert "get animation from enemy."
    This reverts commit d2e116cbbcc4129c9c1b233a4acd3a4670d5c37e.

--- Assets/3DGamekitLite/Scripts/Runtime/Game/Weapon/PlayerWeaponController.cs
+++ Assets/3DGamekitLite/Scripts/Runtime/Game/Weapon/PlayerWeaponController.cs
@@ -126,7 +126,7 @@ namespace Gamekit3D
                 _particleSystem.Play();
                 
                 //Get animation from Collider other.
-                Animator enemyAnimator = _humanoidWeaponControllerIdentification.GetHumanoidAnimator();
+                Animator enemyAnimator = other.gameObject.GetComponentInParent<Animator>();
                 Debug.Log($"PlayerWeaponController:OnTriggerEnter:enemyAnimator {enemyAnimator}");
                 Debug.Log($"PlayerWeaponController:OnTriggerEnter:enemyAnimator.GetCurrentAnimatorStateInfo(0).length {enemyAnimator.GetCurrentAnimatorStateInfo(0).length}");
 
